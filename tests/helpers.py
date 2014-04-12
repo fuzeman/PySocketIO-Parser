91e2fd45cfe63caac2a83ba8270b7ea4bee7bcd7
Dean Gardiner <gardiner91@gmail.com>
2014-04-12 12:13:01 +1200

    Fixed problem in helpers.test() where we didn't ensure callbacks were returned

--- tests/helpers.py
+++ tests/helpers.py
@@ -7,19 +7,29 @@ encoder = parser.Encoder()
 
 
 def test(obj):
+    encode_event = Event()
+
     def callback(encodedPackets):
         log.debug('test callback - encodedPackets: %s', encodedPackets)
         decoder = parser.Decoder()
-        event = Event()
+        decode_event = Event()
 
         @decoder.on('decoded')
         def decoded(packet):
             log.debug('test decoded - packet: %s', packet)
 
             assert packet == obj
-            event.set()
+            decode_event.set()
 
         decoder.add(encodedPackets[0])
-        assert event.wait(5)
+
+        # Ensure decode fires
+        assert decode_event.wait(3), "Decoder didn't return a result after 3 seconds"
+
+        encode_event.set()
 
     encoder.encode(obj, callback)
+
+    # Ensure encode fires
+    assert encode_event.wait(3), "Encoder didn't return a result after 3 seconds"
+
